b16bc9daa6804359e7f0bbdf87ae1a9468b46f68
ibne saif <51085968+saifibne@users.noreply.github.com>
2020-11-21 01:17:35 +0530

    20/11/20

--- main.js
+++ main.js
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
 });
 
 app.use(productRoutes);
-app.use(userRoutes); /*testing*/
+app.use(userRoutes);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/ecommerce", {
